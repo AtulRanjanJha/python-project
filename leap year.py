d90007090cf42d1afc6f8fdcb7ebe834eb360865
jhaatul001 <99534159+jhaatul001@users.noreply.github.com>
2024-02-10 22:45:26 +0530

    Add files via upload

---
+++ leap year.py
@@ -0,0 +1,3 @@
+print("Welcome to the pizza hut!")
+input("What type of pizza you would like.\n 1.Chicago style\n 2.Italian style\n 3.New York style\n")
+
---
+++ max min.py
@@ -0,0 +1,6 @@
+student_score = input("Enter the score of the student").split()
+print(student_score)
+
+min=0
+for elements in student_score:
+    min= 
