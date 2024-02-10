815f50d2af533d8260722903a17e4b20b2616d41
jhaatul001 <99534159+jhaatul001@users.noreply.github.com>
2024-02-10 22:58:58 +0530

    Add files via upload

---
+++ Coin_flip_winner.py
@@ -0,0 +1,14 @@
+import random
+
+user_input = int(input("Enter a number 0 for heads and 1 for tails: "))
+random_side = random.randint(0,1)
+winner = random.randint(0,1)
+toss = ["tails", "heads"]
+
+
+if (random_side == user_input):
+    print ("draw")
+elif (random_side == winner) and (user_input != winner):
+    print (f"computer got {toss[random_side]} and you got {toss[user_input]}. You lose.")
+elif (random_side != winner) and (user_input == winner):
+    print (f"computer got {toss[random_side]} and you got {toss[user_input]}. You win!")
